5529dab7830201ba44f958f11d344439fbf4f2ac
balaji2210 <balajiatkuri10@gmail.com>
2023-08-25 19:34:08 +0530

    make textual changes

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
   return (
     <Theme>
       <ToastContainer />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Theme>
   );
 }
