ff75e301a5a82dbb36987a50cd367042ae6db81e
balaji2210 <balajiatkuri10@gmail.com>
2023-08-25 19:25:18 +0530

    fix bugs

--- components/utils/images/index.js
+++ components/utils/images/index.js
@@ -1,3 +1,3 @@
 export { default as Poster } from "./BORCELLE.png";
-export { default as OTPImage } from "./otp.png";
+export { default as OTPImage } from "./otp .png";
 export { default as student } from "./student.png";
